099d4992395661902ec12392aaa14aaaa26df085
Siddharth Arvind <sidarvind11@gmail.com>
2024-03-05 19:39:10 +0530

    Print 'Webhook enabled' to output console

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,5 +5,6 @@ int main(void) {
   std::cout << "Hello, World\n";
   std::cout << "Hello, Jenkins\n";
   std::cout << "I have successfully built and run\n";
+  std::cout << "GitHub-Ngrok-Jenkins webhook enabled!\n";
   return 0;
 }
