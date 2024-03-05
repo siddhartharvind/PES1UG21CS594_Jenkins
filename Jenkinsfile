siddhartharvind/PES1UG21CS594_Jenkins

commit f3ec5a723519b469e1a8f6a4f96cad5587abe7c8

Update Jenkinsfile - introduce an error for testing
We edit the Jenkinsfile to introduce a deliberate error in one of the stage's steps. We invoke the non-existent command `echp` instead of the intended `echo`.

This will test whether the error in the Jenkins Pipeline is caught and a suitable output is printed to the console.

Jenkinsfile
@@ -15,7 +15,10 @@ pipeline {
     }
     stage ('Deploy') {
       steps {
-        echo 'Deployment successful!'
+        // Introducing an error into the Jenkinsfile!
+        // Command `echp` does not exist.
+        // Intended command: `echo`
+        echp 'Deployment successful!'
       }
     }
   }

commit 6c1ff03c0d2dd57f5a3278a5c924ace0b58b639d

Update Jenkinsfile - fix error introduced in `f3ec5a7`
We confirmed that the error introduced in the previous commit `f3ec5a7` into the Jenkins Pipeline was successfully caught and handled by the Jenkinsfile post-action logic.

This commit restores the original working statement. The error is left but commented out.

Jenkinsfile
@@ -15,10 +15,13 @@ pipeline {
     }
     stage ('Deploy') {
       steps {
+        echo 'Deployment successful!'
+        /*
         // Introducing an error into the Jenkinsfile!
         // Command `echp` does not exist.
         // Intended command: `echo`
         echp 'Deployment successful!'
+        */
       }
     }
   }