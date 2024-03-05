3a49aeb8a8038a9521d1ead44d408030c8c0c760
Siddharth Arvind <sidarvind11@gmail.com>
2024-03-05 19:13:55 +0530

    Remove `using namespace std` to trigger GitHub-Jenkins webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,9 @@
 //File: hello.cpp
 #include <iostream>
-using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  std::cout << "Hello, World\n";
+  std::cout << "Hello, Jenkins\n";
+  std::cout << "I have successfully built and run\n";
   return 0;
 }
